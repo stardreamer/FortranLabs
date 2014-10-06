7567ec89bd80ad66d4d4e18aa89f5fc9791e30c2
Arthur Asylgareev <son.of.gallifrei@gmail.com>
2014-10-06 16:00:31 +0400

    explicit solution

--- Lab2/mod/2simple_algorithm.f90
+++ Lab2/mod/2simple_algorithm.f90
@@ -14,7 +14,75 @@ module simple_algorithm
         
         diff = maxval(abs(sol1 % current_slice % values - sol2 % current_slice % values))
     end function compare_sol
-     
+    !calculate implicit solution
+    subroutine get_implicit_solution(conf, answer, last_timeslice)
+        type(resultdata) :: answer
+        type(timeslice)  :: last_timeslice
+        integer :: i = 1, ntime = 2, maxi = 0
+        type(configuration), intent(in) :: conf
+        real, dimension(1 : (size(last_timeslice % current_slice % x) -1 )) :: A, B
+        
+         do 
+             last_timeslice % time = last_timeslice % time + conf % timestep 
+             i = 2 
+             
+             
+            if (ntime > size(answer % calc_result)) then
+                call extend_result(answer, 2)
+            end if
+            
+            answer % calc_result(ntime) = last_timeslice
+            
+            if (compare_sol(last_timeslice , answer % calc_result(ntime -1)) < conf % eps) then
+                exit
+            end if
+            ntime = ntime + 1
+         end do 
+            
+        
+    end subroutine get_implicit_solution
+    !calculate explicit solution
+    subroutine get_explicit_solution(conf, answer, last_timeslice)
+        type(resultdata) :: answer
+        type(timeslice)  :: last_timeslice
+        type(configuration), intent(in) :: conf
+        integer :: i = 1, ntime = 2, maxi = 0
+        real :: corr_parameter = 0
+        
+        corr_parameter = (conf % alpha * conf % timestep / (conf % step ** 2))
+        
+        maxi = size(last_timeslice % current_slice % x)
+        last_timeslice % current_slice % values(1) = 0
+        last_timeslice % current_slice % values(maxi) = 0
+        
+        do 
+            last_timeslice % time = last_timeslice % time + conf % timestep            
+            
+            i = 2
+            
+            do while (i < maxi)
+                last_timeslice % current_slice % values(i) = answer % calc_result(ntime -1) % current_slice % values(i) + &
+                & corr_parameter * ( &
+                & answer % calc_result(ntime -1) % current_slice % values(i + 1) - &
+                & 2. * answer % calc_result(ntime -1) % current_slice % values(i) +&
+                & answer % calc_result(ntime -1) % current_slice % values(i - 1)&
+                & )
+                i = i + 1 
+            end do
+            
+            if (ntime > size(answer % calc_result)) then
+                call extend_result(answer, 2)
+            end if
+            
+            answer % calc_result(ntime) = last_timeslice
+            
+            if (compare_sol(last_timeslice , answer % calc_result(ntime -1)) < conf % eps) then
+                exit
+            end if
+            ntime = ntime + 1
+        end do
+        print *, ntime
+    end subroutine get_explicit_solution
     !calculates analitical solution
     subroutine get_analitical_solution(conf, fint, answer, last_timeslice)
         type(resultdata) :: answer
@@ -67,7 +135,6 @@ module simple_algorithm
             ntime = ntime + 1
             
         end do
-        print *, ntime
     end subroutine get_analitical_solution
   
     subroutine calculate_solution(conf, answer, f, fint)
@@ -101,7 +168,7 @@ module simple_algorithm
           
                     
           !Setting up start values
-          xgrid = (/(a * conf % step, a=1, conf % numslice)/)  
+          xgrid = (/((a-1) * conf % step, a=1, conf % numslice)/)  
           local_timeslice % time = 0.
           local_timeslice % current_slice % x => xgrid  
           local_timeslice % current_slice % values = (/(f(local_timeslice % current_slice % x(a)), a=1, conf % numslice)/)
@@ -110,7 +177,16 @@ module simple_algorithm
           answer % calc_result(1) = local_timeslice
           
           !Calculating solution
-          call get_analitical_solution(conf, fint, answer, local_timeslice)
+          select case (conf % mode)
+          case (0)
+            call get_analitical_solution(conf, fint, answer, local_timeslice)
+          case (1)
+            call get_explicit_solution(conf, answer, local_timeslice)
+          case default
+            errorcode = SMT_BAD
+          end select
+          !call get_analitical_solution(conf, fint, answer, local_timeslice)
+          !call get_explicit_solution(conf, answer, local_timeslice)
           
     end subroutine calculate_solution 
     
